699e8a7d2eb3d91ae00b0c75a942514bdaeba398
Ivan <77185900+wpinrui@users.noreply.github.com>
2025-12-04 15:37:03 +0800

    fix: use PostCSS plugin for Electron Forge compatibility

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// eslint-disable-next-line import/no-unresolved
-import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
 });
