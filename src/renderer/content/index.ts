70a6705bc3faae93aea15faa8b13f6e342d97949
Ivan <77185900+wpinrui@users.noreply.github.com>
2025-12-06 13:09:35 +0800

    refactor: remove unused ActionConfig export

--- src/renderer/content/index.ts
+++ src/renderer/content/index.ts
@@ -1,4 +1,4 @@
 export { TeamProfile } from './TeamProfile';
 export { SavedGames } from './SavedGames';
 export { GameOptions } from './GameOptions';
-export { ActionScreen, ACTION_CONFIGS, isActionType, type ActionType, type ActionConfig } from './ActionScreen';
+export { ActionScreen, ACTION_CONFIGS, isActionType, type ActionType } from './ActionScreen';
