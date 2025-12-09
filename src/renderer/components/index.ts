57dd03efefd6c3b5216790e894e85283d557b522
Ivan <77185900+wpinrui@users.noreply.github.com>
2025-12-09 12:27:10 +0800

    feat: wire up EntityNavigationContext in MainLayout

--- src/renderer/components/index.ts
+++ src/renderer/components/index.ts
@@ -6,3 +6,4 @@ export { HeaderCell } from './table';
 export { SummaryStat, DetailRow, ProgressBar, CenteredMessage, getPercentageColorClass, TabBar } from './ContentPrimitives';
 export type { Tab } from './ContentPrimitives';
 export { CarViewer3D } from './CarViewer3D';
+export { EntityLink } from './EntityLink';
